06e9ba9ce97671ac402fd4d5e82b665264c0d77b
狐瓜西 <SuikaKitsune@huguaxi.com>
2026-06-08 20:18:27 +0800

    V1.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <script setup>
+import { onBeforeUnmount, onMounted, shallowRef } from 'vue'
 import HomePage from './components/HomePage.vue'
+import OpeningTextWall from './components/OpeningTextWall.vue'
+
+const showOpeningTextWall = shallowRef(true)
+let openingTextWallTimer
+
+onMounted(() => {
+  openingTextWallTimer = window.setTimeout(() => {
+    showOpeningTextWall.value = false
+  }, 1600)
+})
+
+onBeforeUnmount(() => {
+  window.clearTimeout(openingTextWallTimer)
+})
 </script>
 
 <template>
   <HomePage />
+
+  <Transition name="opening-text-wall" appear>
+    <OpeningTextWall v-if="showOpeningTextWall" />
+  </Transition>
 </template>
